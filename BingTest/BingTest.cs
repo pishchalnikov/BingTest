333cf2534f5cd606619a39dede943e5d44488ce4
Roman Pishchalnikov <roman.pishchalnikov@gmail.com>
2015-02-16 12:57:25 +0600

    init

---
+++ BingTest/BingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace BingTest
+{
+    public class BingTest
+    {
+        private FirefoxDriver driver;
+
+        [SetUp]
+        public void SetUo()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("http://www.bing.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
